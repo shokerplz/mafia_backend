786692860320468973013accc92048a7f8672f66
Ivan Kovalev <shoker444@gmail.com>
2020-11-21 01:03:18 +0300

    Add room json preload

---
+++ main.py
@@ -0,0 +1,40 @@
+from importing_modules import *
+app = Flask(__name__)
+
+global players, rooms
+
+players = []
+rooms   = {}
+room_json = json.load(open("template_room.json"))
+user_json = json.load(open("template_user.json"))
+
+@app.route('/')
+def root():
+    return("Project root")
+
+@app.route('/get-user-id', methods=['POST'])
+def get_user_id():
+    while True:
+        player_id = random.randint(0, 100)
+        if player_id not in players:
+            players.append(player_id)
+        else: break
+    return(jsonify(
+        USER_ID=player_id
+))
+
+@app.route('/create', methods=['POST'])
+def create_root():
+    data = request.args
+    if (not data or not data['user_id']): abort(400, 'User id not found')
+    else: user_id = data['user_id']
+    while True:
+        room_id = random.randint(0, 100)
+        if room_id not in rooms.keys():
+            rooms[room_id] = dict(room_json)
+            rooms[room_id]['id'] = room_id
+            break # Create function to enter new room
+    return(jsonify(
+        rooms[room_id]
+    ))
+app.run()
