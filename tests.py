487a16f4fbfc772fbd9963cd18897eedec32fadb
Ivan Kovalev <shoker444@gmail.com>
2020-11-26 23:24:26 +0300

    Set nicer test output

--- tests.py
+++ tests.py
@@ -25,24 +25,31 @@ def get_status(id):
     return(joined_json)
 
 def vote(user_id, target_id):
-    print('http://localhost:5000/action?action=vote&user_id='+user_id+'&target_id='+target_id)
+    #print('http://localhost:5000/action?action=vote&user_id='+user_id+'&target_id='+target_id)
     try:
         r = requests.post('http://localhost:5000/action?action=vote&user_id='+user_id+'&target_id='+target_id)
         voted_json = r.json()
     except:
-        print(r.text)
+        #print(r.text)
         voted_json = r.text
     return(voted_json)
 
 def kill(user_id, target_id):
-    print('http://localhost:5000/action?action=kill&user_id='+user_id+'&target_id='+target_id)
+    #print('http://localhost:5000/action?action=kill&user_id='+user_id+'&target_id='+target_id)
     try:
         r = requests.post('http://localhost:5000/action?action=kill&user_id='+user_id+'&target_id='+target_id)
         voted_to_kill_json = r.json()
     except:
-        print(r.text)
+        #print(r.text)
         voted_to_kill_json = r.text
     return(voted_to_kill_json)
+
+def print_status(room_status):
+    print('Alive users: '+' '.join(room_status['alive']))
+    print('Alive mafia: '+' '.join(room_status['mafia']))
+    print('Alive peaceful: '+' '.join(room_status['peaceful']))
+    print('Jailed users: '+' '.join(room_status['jailed']))
+    print('Killed users: '+' '.join(room_status['killed']))
 
 #try:
 USERS_IN_ROOM=9
@@ -58,16 +65,16 @@ for user in users_in_room:
 time.sleep(10)
 room_status = json.loads(get_status(users_in_room[1]))
 print('ROOM STATUS ON FIRST READY: ')
-print(room_status)
+print_status(room_status)
 print('\n')
 for player in room_status['users']:
     if (player['role'] == 'mafia'):
-        print(player['id'])
+        #print(player['id'])
         set_ready(player['id'])
 time.sleep(15)
 room_status = json.loads(get_status(users_in_room[1]))
 print('ROOM STATUS ON MAFIA READY: ')
-print(room_status)
+print_status(room_status)
 print('\n')
 while not room_status['state'].startswith('ended'):
     time.sleep(5)
@@ -76,6 +83,7 @@ while not room_status['state'].startswith('ended'):
         to_vote = random.choice(users_in_room)
         if to_vote in room_status['alive']: break
         else: continue
+    print('All users will vote to: '+to_vote)
     room_status = json.loads(get_status(users_in_room[0]))
     for user in users_in_room:
         if (user in room_status['alive']):
@@ -83,7 +91,7 @@ while not room_status['state'].startswith('ended'):
     time.sleep(5)
     room_status = json.loads(get_status(users_in_room[1]))
     print('ROOM STATUS AFTER VOTE: ')
-    print(room_status)
+    print_status(room_status)
     print('\n')
     time.sleep(5)
     room_status = json.loads(get_status(users_in_room[0]))
@@ -91,6 +99,7 @@ while not room_status['state'].startswith('ended'):
         to_kill = random.choice(users_in_room)
         if to_kill in room_status['alive']: break
         else: continue
+    print('All mafia will kill: '+to_kill)
     room_status = json.loads(get_status(users_in_room[0]))
     for player in room_status['users']:
         if (player['role'] == 'mafia' and player['id'] in room_status['alive']):
@@ -98,8 +107,11 @@ while not room_status['state'].startswith('ended'):
     time.sleep(5)
     room_status = json.loads(get_status(users_in_room[0]))
     print('ROOM STATUS AFTER KILL: ')
-    print(room_status)
+    print_status(room_status)
     print('\n')
+if ('peaceful' in room_status['state']):
+    print('Peaceful won')
+else: print('Mafia won')
         
 
 #except Exception as e:
